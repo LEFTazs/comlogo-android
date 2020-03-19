91b400c61efed3a6b065fbfc00bee9415abf5714
LEFTazs <leftazshun@gmail.com>
2020-03-19 20:12:43 +0100

    Import and use FixedSys font

--- app/src/main/java/hu/pannon/mik/comlogoandroid/MainActivity.kt
+++ app/src/main/java/hu/pannon/mik/comlogoandroid/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ScrollView
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
 
