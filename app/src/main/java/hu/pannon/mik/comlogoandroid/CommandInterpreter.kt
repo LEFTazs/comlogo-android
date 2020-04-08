08ead86f205a225c6c60a80d90a3a98016b7e017
LEFTazs <leftazshun@gmail.com>
2020-04-08 16:30:20 +0200

    Add new commands and refractor CommandInterpreter

--- app/src/main/java/hu/pannon/mik/comlogoandroid/CommandInterpreter.kt
+++ app/src/main/java/hu/pannon/mik/comlogoandroid/CommandInterpreter.kt
@@ -1,50 +1,111 @@
 package hu.pannon.mik.comlogoandroid
 
 import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 class CommandInterpreter {
     var canvasState = CanvasState()
 
+    private lateinit var commandIterator: Iterator<String>
+
     fun interpret(entry: String) {
         val commandParts = entry.split(" ")
-        val iterator = commandParts.iterator()
-        while (iterator.hasNext()) {
-            val lastX = canvasState.turtle.x.toFloat()
-            val lastY = canvasState.turtle.y.toFloat()
-
-            val command = iterator.next()
-            when (command) {
-                "előre", "e" -> commandMoveForward(iterator)
-                "hátra", "h" -> commandMoveBackwards(iterator)
-                "jobbra", "j" -> commandTurnRight(iterator)
-                "balra", "b" -> commandTurnLeft(iterator)
-            }
-
-            val newX = canvasState.turtle.x.toFloat()
-            val newY = canvasState.turtle.y.toFloat()
-            val newLine = Line(lastX, lastY, newX, newY, canvasState.penStrokeWidth, canvasState.penColor)
-            if (canvasState.isPenDown)
-                canvasState.lines.add(newLine)
+        commandIterator = commandParts.iterator()
+        while (commandIterator.hasNext()) {
+            executeCommand()
         }
     }
 
-    private fun commandMoveForward(iterator: Iterator<String>) {
-        val distance = iterator.next().toDouble()
+    private fun executeCommand() {
+        val command = commandIterator.next()
+        when (command) {
+            "előre", "e" -> commandMoveForward()
+            "hátra", "h" -> commandMoveBackwards()
+            "jobbra", "j" -> commandTurnRight()
+            "balra", "b" -> commandTurnLeft()
+            "tollatle", "tl" -> commandPenOn()
+            "tollatfel", "tf" -> commandPenOff()
+            "törölrajzlap", "tr", "törölképernyő", "törölkép" -> commandClearCanvas()
+            "ismétlés", "ism" -> commandRepeat()
+        }
+    }
+
+    private fun commandMoveForward() {
+        val lastX = canvasState.turtle.x.toFloat()
+        val lastY = canvasState.turtle.y.toFloat()
+
+        val distance = commandIterator.next().toDouble()
         canvasState.turtle.moveForward(distance)
+
+        val newX = canvasState.turtle.x.toFloat()
+        val newY = canvasState.turtle.y.toFloat()
+        val newLine = Line(lastX, lastY, newX, newY, canvasState.penStrokeWidth, canvasState.penColor)
+        if (canvasState.isPenDown)
+            canvasState.lines.add(newLine)
     }
 
-    private fun commandMoveBackwards(iterator: Iterator<String>) {
-        val distance = iterator.next().toDouble() * -1
+    private fun commandMoveBackwards() {
+        val lastX = canvasState.turtle.x.toFloat()
+        val lastY = canvasState.turtle.y.toFloat()
+
+        val distance = commandIterator.next().toDouble() * -1
         canvasState.turtle.moveForward(distance)
+
+        val newX = canvasState.turtle.x.toFloat()
+        val newY = canvasState.turtle.y.toFloat()
+        val newLine = Line(lastX, lastY, newX, newY, canvasState.penStrokeWidth, canvasState.penColor)
+        if (canvasState.isPenDown)
+            canvasState.lines.add(newLine)
     }
 
-    private fun commandTurnRight(iterator: Iterator<String>) {
-        val degrees = iterator.next().toDouble()
+    private fun commandTurnRight() {
+        val lastX = canvasState.turtle.x.toFloat()
+        val lastY = canvasState.turtle.y.toFloat()
+
+        val degrees = commandIterator.next().toDouble()
         canvasState.turtle.rotate(degrees)
+
+        val newX = canvasState.turtle.x.toFloat()
+        val newY = canvasState.turtle.y.toFloat()
+        val newLine = Line(lastX, lastY, newX, newY, canvasState.penStrokeWidth, canvasState.penColor)
+        if (canvasState.isPenDown)
+            canvasState.lines.add(newLine)
     }
 
-    private fun commandTurnLeft(iterator: Iterator<String>) {
-        val degrees = iterator.next().toDouble() * -1
+    private fun commandTurnLeft() {
+        val lastX = canvasState.turtle.x.toFloat()
+        val lastY = canvasState.turtle.y.toFloat()
+
+        val degrees = commandIterator.next().toDouble() * -1
         canvasState.turtle.rotate(degrees)
+
+        val newX = canvasState.turtle.x.toFloat()
+        val newY = canvasState.turtle.y.toFloat()
+        val newLine = Line(lastX, lastY, newX, newY, canvasState.penStrokeWidth, canvasState.penColor)
+        if (canvasState.isPenDown)
+            canvasState.lines.add(newLine)
+    }
+
+    private fun commandPenOn() {
+        canvasState.isPenDown = true
+    }
+
+    private fun commandPenOff() {
+        canvasState.isPenDown = false
+    }
+
+    private fun commandClearCanvas() {
+        canvasState.lines.clear()
+    }
+
+    private fun commandRepeat() {
+        val times = commandIterator.next().toInt()
+        val remainingCommands = commandIterator.asSequence().joinToString(" ")
+        val originalIterator = commandIterator
+        for (i in 0..times) {
+            interpret(remainingCommands)
+        }
+        commandIterator = originalIterator
     }
 }
